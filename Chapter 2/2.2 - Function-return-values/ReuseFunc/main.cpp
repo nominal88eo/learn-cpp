ee96242a92ee8f4f63b62014304988a296dc257e
Erkan Ozvatan <nominal88.eo@gmail.com>
2024-12-08 16:39:46 +0200

    Add project files

---
+++ Chapter 2/2.2 - Function-return-values/ReuseFunc/main.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+
+// Use DRY principle (Don't repeat yourself)
+int getValueFromUser()
+{
+  std::cout << "Enter an integer: ";
+  int input{};
+  std::cin >> input;
+
+  return input;
+}
+
+int main()
+{
+  int x{ getValueFromUser() }; // first call to getValueFromUser
+  int y{ getValueFromUser() }; // second call to getValueFromUser
+
+  std::cout << x << " + " << y << " = " << x + y << '\n';
+
+  return 0;
+}
